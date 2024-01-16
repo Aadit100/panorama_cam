11e344459ca5c7624f6e9f3cb69afb3bcd67ae7a
Aadit100 <130185213+Aadit100@users.noreply.github.com>
2024-01-16 15:19:49 +0530

    Amogh's commit before correcting servo2

--- pan_tilt/pan_tilt.ino
+++ pan_tilt/pan_tilt.ino
@@ -1,18 +1,25 @@
 #include <ESP32Servo.h>
 #include <ros.h>
 #include <sensor_msgs/Joy.h>
+
 ros::NodeHandle nh;
+
 Servo servo1;  //pan motor
 Servo servo2;  //tilt motor
-int servoPin1=4;  //attach pan motor to pin4
-int servoPin2=0;  //attach tilt motor to pin0
+#define servoPin1 4  //attach pan motor to pin4
+#define servoPin2 0  //attach tilt motor to pin0
+
 int ADC_Max=4096;
 int init_pan=96;
 int init_tilt=80;
+
 int val1=init_pan;  //this is for pan motor so this is the value where it does not rotate
 int val2=init_tilt; //this is the value for tilt motor so value is 0 initially
+
+//feed is taken directly from the joy and val is calculated from feed (and eventually written to the servo)
 float feed1=0;
 float feed2=0;
+
 ros::Subscriber<sensor_msgs::Joy> sub("joy",&joyCallback);
 
 void joyCallback(const sensor_msgs::Joy& msg){
@@ -45,11 +52,7 @@ void setup(){
   servo2.attach(servoPin2);
   nh.getHardware()->setBaud(115200);
   nh.initNode();
-  nh.subscribe(commands);
-}
-
-void setup(){
-  
+  nh.subscribe(sub);
 }
 
 void loop(){
